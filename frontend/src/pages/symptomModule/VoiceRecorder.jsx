43ace3b6d47156666150c4f6400c4f5bc0b89dd4
SKShanthakumar <skshanthakumar181004@gmail.com>
2025-04-11 19:11:04 +0530

    -

---
+++ frontend/src/pages/symptomModule/VoiceRecorder.jsx
@@ -0,0 +1,19 @@
+import { ReactMediaRecorder } from "react-media-recorder";
+
+export default function VoiceRecorder() {
+    return (
+        <div className="flex flex-col items-center space-y-4 mt-96">
+            <ReactMediaRecorder
+                audio
+                render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                    <div>
+                        <p>{status}</p>
+                        <button onClick={startRecording}>Start Recording</button>
+                        <button onClick={stopRecording}>Stop Recording.</button>
+                        <audio src={mediaBlobUrl} controls autoPlay loop />
+                    </div>
+                )}
+            />
+        </div>
+    )
+};
